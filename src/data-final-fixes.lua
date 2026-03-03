aba1c573ff69b7f9327c3930393ea694cb34395a
John Call <jcall.engineer@gmail.com>
2026-03-03 12:49:01 -0700

    Fix final bug with percentage allocation and add localization for setting

--- src/data-final-fixes.lua
+++ src/data-final-fixes.lua
@@ -1,7 +1,13 @@
 local quality_multipliers = { 1, 1.3, 1.6, 1.9, 2.5 }
 local module_bases = { 1, 2, 2.5 }
 local module_names = { "quality-module", "quality-module-2", "quality-module-3" }
-local target_q3_legendary = settings.startup["target-q3-legendary-quality"].value
+
+--[[
+In Factorio, quality is expressed as /10 instead of /100
+This is due to the precision of type EventValue limited to two decimal places
+And quality often works with half percents which would require a third digit when expressed as a raw decimal value
+]]
+local target_q3_legendary = settings.startup["target-q3-legendary-quality-percent"].value / 10
 
 local normal_base = target_q3_legendary / (quality_multipliers[5] * module_bases[3])
 
--- src/settings.lua
+++ src/settings.lua
@@ -1,10 +1,10 @@
 data:extend({
 	{
 		type = "double-setting",
-		name = "target-q3-legendary-quality",
+		name = "target-q3-legendary-quality-percent",
 		setting_type = "startup",
-		default_value = 0.25,
+		default_value = 25,
 		minimum_value = 0,
-		maximum_value = 1,
+		maximum_value = 100,
 	}
 })
